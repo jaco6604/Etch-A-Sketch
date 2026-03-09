2c70f485898a8465fcda69b5e3e6be45d524ceb8
Ryan Jacobson <ryanjacobson1218@gmail.com>
2026-03-08 22:55:49 -0400

    Generate 16x16 grid with JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector("#grid");
+for (let i = 0; i < 256; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");  
+    grid.appendChild(square);
+
+}
